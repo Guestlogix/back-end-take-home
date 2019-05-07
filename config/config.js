3bde76a8171f9dd9a5b4778c1f9a964cbac10f5e
Susan Adelokiki <susan.adelokiki@andela.com>
2019-05-08 00:38:41 +0100

    Add local mongo URI

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'database': 'mongodb://heroku_mm5sglp5:7lfl3nh8pdvi1pg28db42l5dgl@ds153394.mlab.com:53394/heroku_mm5sglp5'
+  'database': 'mongodb://localhost:27017/flights'
 }
