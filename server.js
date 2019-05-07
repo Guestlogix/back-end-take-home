73bbdd0b867f6bef180c76a03132b4d90e42b4ee
Susan Adelokiki <susan.adelokiki@andela.com>
2019-05-07 23:04:57 +0100

    Update port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var app = require("./config/express")();
 const config = require("./config/config");
-const port = config.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log("App running live on: " + port);
