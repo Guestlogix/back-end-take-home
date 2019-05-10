ecb05440f8f0e2fcc9b3dfd48088379b3703c4a9
Aden <adentong@hotmail.com>
2019-05-09 22:57:55 -0400

    add unit tests for incomplete method

--- Guestlogix.Tests/Models/AirNetworkTests.cs
+++ Guestlogix.Tests/Models/AirNetworkTests.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Guestlogix.Models;
-using System;
+﻿using Guestlogix.exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Guestlogix.Models.Tests
 {
@@ -16,19 +12,72 @@ namespace Guestlogix.Models.Tests
         [TestInitialize]
         public void TestInitialize()
         {
-
+            List<AirportModel> airports = new List<AirportModel>();
+            airports.Add(new AirportModel { IATA3 = "A1" });
+            airports.Add(new AirportModel { IATA3 = "A2" });
+            airports.Add(new AirportModel { IATA3 = "A3" });
+            airports.Add(new AirportModel { IATA3 = "A4" });
+            List<FlightModel> flights = new List<FlightModel>();
+            flights.Add(new FlightModel { Origin = "A1", Destination = "A2" });
+            flights.Add(new FlightModel { Origin = "A1", Destination = "A3" });
+            flights.Add(new FlightModel { Origin = "A2", Destination = "A3" });
+            flights.Add(new FlightModel { Origin = "A2", Destination = "A4" });
+            airNetwork = new AirNetwork(null, airports, flights);
         }
 
         [TestMethod]
-        public void GetShortestRouteTest()
+        [ExpectedException(typeof(OriginNotFoundException))]
+        public void GetShortestRoute_OriginNotFound_KnownDestination()
         {
-            Assert.Fail();
+            airNetwork.GetShortestRoute("NotFound", "A1");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OriginNotFoundException))]
+        public void GetShortestRoute_OriginNotFound_DestinationNotFound()
+        {
+            airNetwork.GetShortestRoute("NotFound", "NotFound");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DestinationNotFoundException))]
+        public void GetShortestRoute_KnownOrigin_DestinationNotFound()
+        {
+            airNetwork.GetShortestRoute("A1", "NotFound");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NoRouteFoundException))]
+        public void GetShortestRoute_NoRouteFound()
+        {
+            airNetwork.GetShortestRoute("A3", "A4");
+        }
+
+        [TestMethod]
+        public void GetShortestRoute_OneRoute()
+        {
+            RouteModel route = airNetwork.GetShortestRoute("A1", "A4");
+            Assert.AreEqual(route.Flights.Count, 2);
+            Assert.AreEqual(route.Flights[0].Origin, "A1");
+            Assert.AreEqual(route.Flights[0].Destination, "A2");
+            Assert.AreEqual(route.Flights[1].Origin, "A2");
+            Assert.AreEqual(route.Flights[1].Destination, "A4");
+        }
+
+        [TestMethod]
+        public void GetShortestRoute_TwoDifferentRoutes()
+        {
+            RouteModel route = airNetwork.GetShortestRoute("A1", "A3");
+            Assert.AreEqual(route.Flights.Count, 1);
+            Assert.AreNotEqual(route.Flights.Count, 2);
+            Assert.AreEqual(route.Flights[0].Origin, "A1");
+            Assert.AreEqual(route.Flights[0].Destination, "A3");
         }
 
         [TestCleanup]
         public void TestCleanup()
         {
-
+            airNetwork = null;
         }
     }
 }
