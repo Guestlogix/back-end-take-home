fc4ae6697dc3a8cd41b305c02e5ce0d9efe8cf75
irysius <lestersy@hotmail.com>
2019-07-05 22:10:13 -0400

    finish writing out the store's "invalid" tests

--- shared/shortest-path.ts
+++ shared/shortest-path.ts
@@ -15,6 +15,12 @@ export type ShortestPathResults = ShortestPathNodes|ShortestPathError;
 export interface ShortestPathCalculatorOptions {
 
 }
+export interface IShortestPathCalculator {
+	/**
+	 * Given origin and destination, attempt to calculate the shortest path between them and return the results.
+	 */
+	calculate(param: ShortestPathParameters): ShortestPathResults;
+}
 
 /**
  * Helper function to determine if value can be considered a ShortestPathNodes object.
@@ -26,10 +32,8 @@ export function isShortestPathNodes(value): value is ShortestPathNodes {
 /**
  * Creates a shortest path calculator using the given information in options.
  */
-export function ShortestPathCalculator(options: ShortestPathCalculatorOptions) {
-	/**
-	 * Given origin and destination, attempt to calculate the shortest path between them and return the results.
-	 */
+export function ShortestPathCalculator(options: ShortestPathCalculatorOptions): IShortestPathCalculator {
+	
 	function calculate(param: ShortestPathParameters): ShortestPathResults {
 		const { origin, destination } = param;
 		throw new Error('Not Implemented Yet');
--- test/shortest-path.ts
+++ test/shortest-path.ts
@@ -1,17 +1,64 @@
 import { expect } from 'chai';
-import { ShortestPathCalculator } from '../shared/shortest-path';
+import { ShortestPathCalculator, IShortestPathCalculator, ShortestPathError } from '../shared/shortest-path';
+import { Store } from '../server/store';
 describe('shortest path', () => {
-	const pathCalculator = ShortestPathCalculator({});
+	let pathCalculator: IShortestPathCalculator;
+	before(() => {
+		let store = Store({
+			airlinesCsv: './data/test/airlines.csv',
+			airportsCsv: './data/test/airports.csv',
+			routesCsv: './data/test/routes.csv'
+		});
+		pathCalculator = ShortestPathCalculator({ store });
+	});
 	it('is expected to detect if the origin is invalid', () => {
-
+		const invalidParams = [
+			{ destination: 'JFK' },
+			{ origin: null, destination: 'JFK' },
+			{ origin: '', destination: 'JFK' },
+			{ origin: 'XXX', destination: 'JFK' }
+		];
+		
+		const expected = ShortestPathError.InvalidOrigin;
+		const results = invalidParams.map(pathCalculator.calculate);
+		let i;
+		for (i = 0; i < results.length; ++i) {
+			const result = results[i];
+			expect(result).to.equal(expected, `expected ${result} to match ShortestPathError.InvalidOrigin (${expected})`);
+		}
+		
 	});
 	it('is expected to detect if the destination is invalid', () => {
-
+		const invalidParams = [
+			{ origin: 'JFK' },
+			{ origin: 'JFK', destination: null },
+			{ origin: 'JFK', destination: '' },
+			{ origin: 'JFK', destination: 'XXX' }
+		];
+		const expected = ShortestPathError.InvalidDestination;
+		const results = invalidParams.map(pathCalculator.calculate);
+		let i;
+		for (i = 0; i < results.length; ++i) {
+			const result = results[i];
+			expect(result).to.equal(expected, `expected ${result} to match ShortestPathError.InvalidDestination (${expected})`);
+		}
 	});
 	it('is expected to detect if both origin and destination are invalid', () => {
-
+		const invalidParams = [
+			{}, { origin: '', destination: '' },
+			{ origin: null, destination: null },
+			{ origin: 'JFK', destination: 'JFK' },
+			{ origin: 'XXX', destination: 'XXX' },
+		];
+		const expected = ShortestPathError.InvalidDestination;
+		const results = invalidParams.map(pathCalculator.calculate);
+		let i;
+		for (i = 0; i < results.length; ++i) {
+			const result = results[i];
+			expect(result).to.equal(expected, `expected ${result}[${i}] to match ShortestPathError.Invalid (${expected})`);
+		}
 	});
-	it('is expected to calculate the route', () => {
+	it.skip('is expected to calculate the route', () => {
 		// TODO: expand the expectations here.
 	});
 });
