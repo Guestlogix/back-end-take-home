1ccb04e3affa8dacfa6ffa4ae3fa3c1d39e1a752
Luidy Naves <luidymorais@gmail.com>
2019-10-07 01:05:30 -0400

    added explanation how to run, disable HTTPS, adde files to run

--- src/RouteSearch.API/Startup.cs
+++ src/RouteSearch.API/Startup.cs
@@ -64,14 +64,8 @@ namespace RouteSearch.API
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseMiddleware<RouteSearchExceptionHandlerMiddleware>();
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
